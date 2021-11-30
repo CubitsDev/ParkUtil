98a7099bfa3eb128d8ec41d578b193e65f89c9a1
Tom Roman <tom@tomroman.co.uk>
2021-11-30 21:11:09 +0000

    Initial Commit

---
+++ src/main/java/dev/tomr/parkutil/Parkutil.java
@@ -0,0 +1,17 @@
+package dev.tomr.parkutil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Parkutil extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
